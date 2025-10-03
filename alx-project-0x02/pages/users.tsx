c8fc30395e95139a106d4d94752e0b5eeb70e0b7
Emmanuel Joseph <emmanueljoseph@Emmanuels-MacBook-Pro.local>
2025-10-03 01:35:25 +0100

    hfhf

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { UseProps } from "@/interfaces";
 import UserCard from "@/components/common/UserCard";
+import { revalidatePath } from "next/cache";
 
 const Users: React.FC = () => {
   const [users, setPosts] = useState<UseProps[]>([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.org/users");
-        const data: UseProps[] = await res.json();
-        console.log(data);
-        setPosts(data.slice(0, 10)); // show only first 10 posts
-      } catch (error) {
-        console.error("Failed to fetch posts:", error);
-      }
+    const getStaticProps = async () => {
+      const res = await fetch("https://jsonplaceholder.org/users");
+      const data: UseProps[] = await res.json();
+      console.log(data);
+      setPosts(data.slice(0, 10)); // show only first 10 posts
+
+      return {
+        props: { users },
+        revalidate: 10,
+      };
     };
 
-    fetchUsers();
+    getStaticProps();
   }, []);
 
   return (
